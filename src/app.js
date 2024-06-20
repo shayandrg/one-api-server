5c8853d0336a1deae75bc83718bf98ec71c57d99
ShayanDRG <shayan.derogarian@gmail.com>
2024-06-20 18:19:45 +0330

    fix: try for production

--- src/app.js
+++ src/app.js
@@ -1,34 +1,26 @@
 const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const httpProxy = require('http-proxy');
 const https = require('https');
 const fs = require('fs');
-const EventEmitter = require('events');
-EventEmitter.defaultMaxListeners = 20;
 
 const app = express();
 const port = 443;
+const proxy = httpProxy.createProxyServer();
 
 app.use(express.json({ limit: '64mb' }));
 
-const proxyOptions = {
-  target: 'http://one-api:3000',
-  changeOrigin: true,
-  onProxyReq: (proxyReq, req, res) => {
-    proxyReq.setHeader('Host', req.headers.host);
-    proxyReq.setHeader('X-Forwarded-For', req.connection.remoteAddress);
-  },
-  onProxyReqWs: (proxyReq, req, socket, options, head) => {
-    proxyReq.setHeader('X-Forwarded-For', req.connection.remoteAddress);
-  },
-};
-
 app.use((req, res, next) => {
-  console.log(`Request received for ${req.hostname}`);
+  console.log(`Request received for ${req.hostname} ${req.method} ${req.url}`);
   if (req.hostname === 'ai.livecdn.website') {
-    createProxyMiddleware(proxyOptions)(req, res, next);
+    proxy.web(req, res, { target: 'http://one-api:3000', changeOrigin: true }, next);
   } else {
     res.status(404).send('Not Found');
   }
+});
+
+proxy.on('proxyReq', (proxyReq, req, res, options) => {
+  proxyReq.setHeader('Host', req.headers.host);
+  proxyReq.setHeader('X-Forwarded-For', req.connection.remoteAddress);
 });
 
 const options = {
